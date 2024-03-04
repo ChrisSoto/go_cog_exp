335706e2263fc2f7219963d33da93734a7bcc6da
chris soto <christopherrsoto@gmail.com>
2024-03-04 09:50:39 -0800

    removed test

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,9 +1,9 @@
 import {  NextResponse } from "next/server"
 
-export async function GET() {
-  return NextResponse.json({ name: "John Doe" })
-}
+// export async function GET() {
+//   return NextResponse.json({ name: "John Doe" })
+// }
 
-export async function POST({ body }) {
-  return NextResponse.json({ name: body.name })
-}
+// export async function POST({ body }) {
+//   return NextResponse.json({ name: body.name })
+// }
